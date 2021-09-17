e1564ffe9c88c952f5938666ec6e59d32a9234e8
AlikaU <alika.utepova@gmail.com>
2021-09-17 01:13:33 -0400

    send put request

--- test_update_player.py
+++ test_update_player.py
@@ -1,17 +1,38 @@
 import responses, json
 from update_player import *
 
+test_url = "http://test"
 
 @responses.activate
 def test_send_put_request():
-    with open("200.json") as f:
-        responses.add(
-            responses.PUT,
-            "http://bla",
-            json=json.load(f),
-            status=200
-        )
-    response = send_put_request()
-    assert response.status_code == 200
-    response_body = response.json()
-    assert response_body["profile"]["applications"][0]["applicationId"] == "music_app"
+    with open("example_data.json") as f:
+        responses.add(responses.PUT, test_url, json=json.load(f), status=200)
+    assert send_put_request(test_url)
+
+
+@responses.activate
+def test_send_put_request_401():
+    with open("401.json") as f:
+        responses.add(responses.PUT, test_url, json=json.load(f), status=401)
+    assert not send_put_request(test_url)
+
+@responses.activate
+def test_send_put_request_handle_HTTPError():
+    responses.add(
+        responses.PUT, test_url, body=requests.exceptions.HTTPError("http error")
+    )
+    assert not send_put_request(test_url)
+
+@responses.activate
+def test_send_put_request_handle_ConnectionError():
+    responses.add(
+        responses.PUT, test_url, body=requests.exceptions.ConnectionError("connection error")
+    )
+    assert not send_put_request(test_url)
+
+@responses.activate
+def test_send_put_request_handle_RequestException():
+    responses.add(
+        responses.PUT, test_url, body=requests.exceptions.RequestException("error while sending request")
+    )
+    assert not send_put_request(test_url)
--- update_player.py
+++ update_player.py
@@ -1,17 +1,40 @@
-import requests
+import requests, json
+
+endpoint = 'http://someendpoint.com'
 
 def main():
-    update_players()
+    update_player()
 
 
-def update_players():
-    pass
+def update_player(macaddress):
+    url = f'{endpoint}/profiles/clientId:{macaddress}'
+    send_put_request(url)
 
 
-def send_put_request():
-    url = "http://bla"
-    resp = requests.put(url)
-    return resp
+def send_put_request(url):
+    client_id, token = get_auth()
+    headers = {'x-client-id': client_id, 'x-authentication-token': token}
+    try:
+        response = requests.put(url, headers=headers, data=get_data())
+        response.raise_for_status()
+        return True
+    except requests.exceptions.HTTPError as err:
+        print(f'http error: {err}')
+    except requests.exceptions.ConnectionError as err:
+        print(f'connection error: {err}')
+    except requests.exceptions.RequestException as err:
+        print(f'error while sending request: {err}')
+    return False
+
+
+def get_auth():
+    return 'some_client_id', 'some_auth_token'
+
+
+def get_data():
+    with open("example_data.json") as f:
+        return json.load(f)
+
 
 
 if __name__ == "__main__":
